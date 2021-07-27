9f10ed1926dfc734632c3d396e31be71c71adfbd
singhkumarhemant <hks9044907610@gmail.com>
2021-07-27 13:47:17 +0530

    added console logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fileSource = './public/rawDataS.json';
 init();
 async function init() {
     try {
+        console.log('Process Initiated');
         await calculateBmi(fileSource);
     } catch (error) {
         console.log('ERROR: ', error);
